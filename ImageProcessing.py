724ade5eba63c254880220a495a341af73097507
Soogbad <soogbad@gmail.com>
2018-10-04 19:25:18 +0300

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -11,37 +11,33 @@ def send(sock, message):
 
 def basic_recv(conn, size):
     size_left = size
-    msg = ""
+    msg = conn.recv(size_left)
     while size_left > 0:
-        if msg != "":
-            msg = msg + conn.recv(size_left)
-        else:
-            msg = conn.recv(size_left)
         size_left = size - len(msg)
+        msg += conn.recv(size_left)
     return msg
 
 
 def recv(conn):
-    size_size = struct.calcsize("L")
-    data_size = struct.unpack("L", basic_recv(conn, size_size))[0]
+    data_size = struct.unpack("L", basic_recv(conn, LONG_SIZE))[0]
     data = basic_recv(conn, data_size)
     return pickle.loads(data)
 
 
-local_host = "127.0.0.1"
-port = 8083
+LONG_SIZE = struct.calcsize("L")
+LOCAL_HOST = "192.168.6.76"
+PORT = 8083
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-is_server = False
 while True:
     answer = input("S for Server, C for Client: ")
     if answer.capitalize() == "S":
-        sock.bind((local_host, port))
+        sock.bind((LOCAL_HOST, PORT))
         print("Waiting for a client...")
         sock.listen(1)
         conn, address = sock.accept()
         print("Successfully created the server!")
         while True:
-            frame, stop = recv(conn)
+            stop, frame = recv(conn)
             if frame is not None:
                 cv2.imshow("Server Frame", frame)
             if stop:
@@ -50,18 +46,18 @@ while True:
         conn.close()
         break
     elif answer.capitalize() == "C":
-        sock.connect((local_host, port))
+        sock.connect((LOCAL_HOST, PORT))
         print("Successfully connected to the server!")
         capture = cv2.VideoCapture(0)
         while True:
             ret, frame = capture.read()
             if ret:
-                send(sock, (frame, False))
+                send(sock, (False, frame))
                 cv2.imshow("Client Frame", frame)
             else:
-                send(sock, (None, False))
+                send(sock, (False, None))
             if ord("q") == cv2.waitKey(1) & 0xFF:
-                send(sock, (None, True))
+                send(sock, (True, None))
                 break
         capture.release()
         break
